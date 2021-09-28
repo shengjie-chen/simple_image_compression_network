02a9bd2b1aef7a717c972eb9bfd82243d7112975
shengjie-chen <chenshengjie1999@126.com>
2021-09-28 10:18:45 +0800

    delete hls project files

--- image_compression_model/solution1/csim.tcl
+++
@@ -1,8 +0,0 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 1986-2020 Xilinx, Inc. All Rights Reserved.
-############################################################
-open_project image_compression_model
-open_solution "solution1"
-csim_design -quiet
