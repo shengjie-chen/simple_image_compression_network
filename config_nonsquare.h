548432be7ebdb950c5daf7fd132ecf591a55efdd
shengjie-chen <chenshengjie1999@126.com>
2021-09-30 21:05:39 +0800

    modify config and fill all weights and bias

--- config_nonsquare.h
+++ config_nonsquare.h
@@ -26,11 +26,11 @@
 #define CONV_1_OFM_ROW 192 
 #define CONV_1_OFM_COL 128 
 #define CONV_1_SIMD 8 
-#define CONV_1_PE 8 
+#define CONV_1_PE 16 
 #define CONV_1_IN_BIT 8 
 #define CONV_1_IN_BIT 8 
 #define CONV_1_W_BIT 4 
-#define CONV_1_W_TILES 6400 
+#define CONV_1_W_TILES 3200 
 
 // conv_2
 #define CONV_2_K 5 
@@ -43,11 +43,11 @@
 #define CONV_2_OFM_ROW 96 
 #define CONV_2_OFM_COL 64
 #define CONV_2_SIMD 8 
-#define CONV_2_PE 8 
+#define CONV_2_PE 16 
 #define CONV_2_IN_BIT 8 
 #define CONV_2_IN_BIT 8 
 #define CONV_2_W_BIT 4 
-#define CONV_2_W_TILES 6400 
+#define CONV_2_W_TILES 3200 
 
 // conv_3
 #define CONV_3_K 5 
@@ -60,11 +60,11 @@
 #define CONV_3_OFM_ROW 48 
 #define CONV_3_OFM_COL 32 
 #define CONV_3_SIMD 12 
-#define CONV_3_PE 8 
+#define CONV_3_PE 16 
 #define CONV_3_IN_BIT 8 
 #define CONV_3_IN_BIT 8 
 #define CONV_3_W_BIT 4 
-#define CONV_3_W_TILES 6400 
+#define CONV_3_W_TILES 3200 
 
 // conv_4
 #define CONV_4_K 5 
@@ -94,11 +94,11 @@
 #define CONV_5_OFM_ROW 192 
 #define CONV_5_OFM_COL 128 
 #define CONV_5_SIMD 8 
-#define CONV_5_PE 8 
+#define CONV_5_PE 16 
 #define CONV_5_IN_BIT 8 
 #define CONV_5_IN_BIT 8 
 #define CONV_5_W_BIT 4 
-#define CONV_5_W_TILES 6400 
+#define CONV_5_W_TILES 3200 
 
 // conv_6
 #define CONV_6_K 5 
@@ -111,11 +111,11 @@
 #define CONV_6_OFM_ROW 384 
 #define CONV_6_OFM_COL 256 
 #define CONV_6_SIMD 8 
-#define CONV_6_PE 8 
+#define CONV_6_PE 16 
 #define CONV_6_IN_BIT 8 
 #define CONV_6_IN_BIT 8 
 #define CONV_6_W_BIT 4 
-#define CONV_6_W_TILES 6400 
+#define CONV_6_W_TILES 3200 
 
 // conv_7
 #define CONV_7_K 5 
